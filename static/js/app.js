14e6d8476d5ecedb97e79bff0d8eeb15b251f2f9
Jacob Field <jacob.field@gmail.com>
2023-10-04 22:52:16 -0400

    updating dropdown works, but uses newPlot instead of restyle

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,6 @@
 // read in data
 let data_url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-
 //initial make functions
 let addToDropdown = (arr, dropdown) => {
   for( n of arr){
@@ -37,6 +36,14 @@ let makeHBar = (sample) => {
   Plotly.newPlot("bar", data_hbar, layout_hbar)
 }
 
+let updateHBar = (sample) => {
+  let h_sample = sample.sample_values.slice(0,10).reverse();
+  let h_otu = sample.otu_ids.slice(0,10).reverse();
+  let h_otuName = sample.otu_labels.slice(0,10).reverse();
+
+  Plotly.restyle("bar", {x: h_sample, y: h_otu, text: h_otuName})
+}
+
 let makeBubble = (sample) => {
   let data_bub = [{
     x: sample.otu_ids,
@@ -70,6 +77,7 @@ let clearDemoTable = (meta_table) => {
   meta_table.text("")
 }
 
+// initial table population
 d3.json(data_url).then((data) => {
   let dropdown = d3.select("#selDataset")
 
@@ -93,16 +101,16 @@ d3.json(data_url).then((data) => {
                 Object.entries(data.metadata[index]))
 })
 
-// // upate plots when new sample is selected
+// upate plots when new sample is selected
 let updatePlotly = () => {
   d3.json(data_url).then((data) => {
     let dropdown = d3.select('#selDataset')
     let val = dropdown.property("value")
     let index = data.names.indexOf(val)
     
-    console.log(index)
-    console.log(data.metadata)
-    console.log(data.metadata[index])
+    console.log(index);
+    console.log(data.metadata[index]);
+    console.log(data.samples[index]);
 
     //update demo table
     let meta_table = d3.select("#sample-metadata")
@@ -111,11 +119,17 @@ let updatePlotly = () => {
                 Object.entries(data.metadata[index]))
 
     //update gauge
-    // Plotly.restyle("gauge", "x", [x]);
+    makeGauge(data.metadata[index]['wfreq']);
+    //TODO: change from make new table to restyle
+    
     //update hbar chart
-    // Plotly.restyle("gauge", "x", [x]);
+    makeHBar(data.samples[index])
+    // updateHBar(data.samples[index])
+    //TODO: change from make new table to restyle
+    
     //update bubble chart
-    // Plotly.restyle("gauge", "x", [x]);
+    makeBubble(data.samples[index])
+    //TODO: change from make new table to restyle
   })
 }
 
