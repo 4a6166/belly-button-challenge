344293f2d378b6ae2cbeeec01537367edab9ac97
Jacob Field <jacob.field@gmail.com>
2023-10-04 21:42:51 -0400

    initial setup working for all visuals

--- static/js/app.js
+++ static/js/app.js
@@ -3,25 +3,16 @@ let data_url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classro
 
 let dropdown = d3.select('#selDataset')
 
-let d;
-d3.json(data_url).then((data) => {
-  d= data; //TODO: remove after testing
-
-  // add vals to dropdown
-  for( n of data.names){
+//initial make functions
+let addToDropdown = (arr) => {
+  for( n of arr){
     dropdown.append("option")
       .text(n)
       .property("value", n);
   }
+}
 
-  let index = 0;
-  // index = dropdown.property("value")
-  let sample = data.samples[index], 
-      metadata = data.metadata[index];
-
-  console.log(sample)
-
-  // // creater horizontal bar chart with dropdown menu to display top 10 OTUs
+let makeHBar = (sample) => {
   let h_sample = sample.sample_values.slice(0,10).reverse();
   let h_otu = sample.otu_ids.slice(0,10).reverse();
   let h_otuName = sample.otu_labels.slice(0,10).reverse();
@@ -33,7 +24,7 @@ d3.json(data_url).then((data) => {
     type: "bar",
     orientation: "h"
   }]
-  
+
   layout_hbar = {
     title: "Top 10 OTUs",
     margine: {
@@ -45,12 +36,64 @@ d3.json(data_url).then((data) => {
   };
 
   Plotly.newPlot("bar", data_hbar, layout_hbar)
-  // // create bubble chart that displays each sample
-  
-  // // display sample metadata (demographic information)
-  
-  // // display each key-value pair from the metadata
+}
 
+let makeBubble = (sample) => {
+  let data_bub = [{
+    x: sample.otu_ids,
+    y: sample.sample_values,
+    text: sample.otu_labels,
+    mode: 'markers',
+    marker: {
+      size: sample.sample_values,
+      color: sample.otu_ids.map(o => o*15)
+    }
+  }];
+
+  let layout_bub = {
+    title: "",
+    showlegend: false,
+    height: 600,
+    width: 1000,
+  };
+  
+  Plotly.newPlot("bubble", data_bub, layout_bub)
+}
+
+let makeDemoTable = (meta_table,  metadata) => {
+  for(l of metadata){
+    let tr = meta_table.append("div");
+    tr.text(`${l[0]}: ${l[1]}\n`);
+  }
+}
+
+d3.json(data_url).then((data) => {
+  // add vals to dropdown
+  addToDropdown(data.names);
+
+  // set initial index to zeo for first load
+  let index = 0;
+  
+  // create gauge (bonus)
+  makeGauge(data.metadata[index]['wfreq']);
+
+  // // creater horizontal bar chart with dropdown menu to display top 10 OTUs
+  makeHBar(data.samples[index]);
+ 
+  // // create bubble chart that displays each sample
+  makeBubble(data.samples[index]);
+
+  // // display sample metadata (demographic information)
+  makeDemoTable(d3.select("#sample-metadata"),
+                Object.entries(data.metadata[index]))
 })
 
 // // upate plots when new sample is selected
+let updatePlotly = () => {
+  let index = dropdown.property("value")
+  console.log(`New entry index: ${index}`)
+
+  makeDemoTable(
+}
+
+dropdown.on("change", updatePlotly);
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,55 +1,54 @@
+let makeGauge = (value) => {
 
-// get value for gauge from data
-let value = 5; //TODO
-
-// make gauge
-let data = [ {
-    domain: { x: [0, 1], y: [0, 1] },
-    value: value,
-    title: { text: "Belly Button Washing Frequency<br><sup>Scrubs Per Week</sup>", 
-             font: { size: 24 } },
-    type: "indicator",
-    mode: "gauge+number",
-    gauge: {
-      // set gauge bar to not appear
-      bar: {
-        line: {
-          width: 0
+  // make gauge
+  let data = [ {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: value,
+      title: { text: "Belly Button Washing Frequency<br><sup>Scrubs Per Week</sup>", 
+               font: { size: 24 } },
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+        // set gauge bar to not appear
+        bar: {
+          line: {
+            width: 0
+          },
+          thickness: 0
         },
-        thickness: 0
+
+        //set range for guage
+        axis: { range: [null, 9] },
+
+        // set range and colors for steps
+        steps: [
+          { range: [0, 1], color: "#F8F3EB" },
+          { range: [1, 2], color: "#F4F1E4" },
+          { range: [2, 3], color: "#E9E6C9" },
+          { range: [3, 4], color: "#E5E8B0" },
+          { range: [4, 5], color: "#D5E599" },
+          { range: [5, 6], color: "#B6CD8F" },
+          { range: [6, 7], color: "#8AC086" },
+          { range: [7, 8], color: "#88BC8D" },
+          { range: [8, 9], color: "#83B588" }
+        ],
+        threshold: { //TODO: remove threshold and replace with dial/needle
+          line: { color: "red", width: 4},
+          thickness: 1,
+          value: value
+        },
       },
+    } ];
 
-      //set range for guage
-      axis: { range: [null, 9] },
+  let layout = { 
+    width: 600,
+    height: 450,
+    margin: { t: 0, b: 0 },
 
-      // set range and colors for steps
-      steps: [
-        { range: [0, 1], color: "#F8F3EB" },
-        { range: [1, 2], color: "#F4F1E4" },
-        { range: [2, 3], color: "#E9E6C9" },
-        { range: [3, 4], color: "#E5E8B0" },
-        { range: [4, 5], color: "#D5E599" },
-        { range: [5, 6], color: "#B6CD8F" },
-        { range: [6, 7], color: "#8AC086" },
-        { range: [7, 8], color: "#88BC8D" },
-        { range: [8, 9], color: "#83B588" }
-      ],
-      threshold: { //TODO: remove threshold and replace with dial/needle
-        line: { color: "red", width: 4},
-        thickness: 1,
-        value: value
-      },
-    },
-  } ];
+  };
 
-let layout = { 
-  width: 600,
-  height: 450,
-  margin: { t: 0, b: 0 },
-
-};
-
-// initial plot
-Plotly.newPlot('gauge', data, layout);
+  // initial plot
+  Plotly.newPlot('gauge', data, layout);
+}
 
 // TODO update plot when selection changes
